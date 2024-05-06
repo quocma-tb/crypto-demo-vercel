d6003d68eb300fb5318605b22750c2a08d8419c7
quocma <quocmv8197@gmail.com>
2024-05-06 19:19:03 +0700

    feat: add crypto currencies data table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,20 +3,19 @@ import BalanceDetail from './components/BalanceDetail';
 import ChartSmallCard from './components/ChartSmallCard';
 import CoinValue from './components/CoinValue';
 import LoginActivity from './components/LoginActivity';
-import RecentTransaction from './components/RecentTransaction';
+import Cryptocurrencies from './components/Cryptocurrencies';
 import UserDetailcard from './components/UserDetailcard';
 
 function Dashboard() {
     return (
         <div className='container-xxl'>
-            <UserDetailcard />
-            <ChartSmallCard />
-            <BalanceDetail />
-            <div className='row g-3 mb-3 row-deck'>
+            {/* <ChartSmallCard /> */}
+            {/* <BalanceDetail /> */}
+            {/* <div className='row g-3 mb-3 row-deck'>
                 <CoinValue />
                 <LoginActivity />
-            </div>
-            <RecentTransaction />
+            </div> */}
+            <Cryptocurrencies />
         </div>
     )
 }
